1ec871f0fb61bd576eecba8f6e3acb5ac80f27a2
mr18 <zhou060806@163.com>
2022-04-06 17:54:10 +0800

    Update i18n.config.js

--- demo/vue2/i18n.config.js
+++ demo/vue2/i18n.config.js
@@ -4,12 +4,7 @@ module.exports = {
   // i18nDir: path.resolve(__dirname, "./i18n"), //国际化配置输出目录
 
   translation: {
-    // en_US 语言包目录
-    en_US: {
-      source: [
-        path.resolve(__dirname, "./i18n/en_US/翻译内容.xlsx"), //翻译文件excel
-        /*,'...'*/
-      ],
-    },
+    //翻译文件excel
+    en_US:[path.resolve(__dirname, "./i18n/en_US/翻译内容.xlsx")],
   },
 };
