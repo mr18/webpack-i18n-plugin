722b190fcaf0504f982e0a3bd9c18d7383ddb1fb
mr18 <zhou060806@163.com>
2022-04-06 17:07:45 +0800

    Update translate.js

--- src/collector/translate.js
+++ src/collector/translate.js
@@ -1,7 +1,7 @@
-const utils = require("./utils");
-const XLSX = require("xlsx");
-const path = require("path");
-const ora = require("ora");
+const utils = require('./utils');
+const XLSX = require('xlsx');
+const path = require('path');
+const ora = require('ora');
 const myOra = ora();
 
 /**
@@ -14,7 +14,7 @@ module.exports = function genTranslateFile(options, oldKeysMap) {
   if (tranKeys && tranKeys.length) {
     tranKeys.forEach((key) => {
       let sourceFiles = options.translation[key].source;
-      if (sourceFiles && typeof sourceFiles === "string") {
+      if (sourceFiles && typeof sourceFiles === 'string') {
         sourceFiles = [sourceFiles];
       }
       let translateObj = {};
@@ -30,7 +30,7 @@ module.exports = function genTranslateFile(options, oldKeysMap) {
               let sheetData = XLSX.utils.sheet_to_json(workbook.Sheets[name]);
               let tempObj = {};
               sheetData.forEach((item) => {
-                tempObj[item.key] = String(item.text || "").replace(/(<\/?)\s*([a-zA-z])+\s*(>)/g, "$1$2$3"); //移除标签空格
+                tempObj[item.key] = String(item.text || '').replace(/(<\/?)\s*([a-zA-z])+\s*(>)/g, '$1$2$3'); //移除标签空格
               });
               Object.assign(translateObj, tempObj);
             });
@@ -51,22 +51,22 @@ module.exports = function genTranslateFile(options, oldKeysMap) {
           xlsxData.push({
             key: key,
             cn: oldKeysMap[key],
-            text: "",
+            text: '',
           });
           jsonData[key] = oldKeysMap[key];
         }
       });
-      let localeCode = "module.exports = " + JSON.stringify(localeResult);
-      let tranPath = path.resolve(options.i18nDir, "./" + key + "/locale.js");
+      let localeCode = 'module.exports = ' + JSON.stringify(localeResult);
+      let tranPath = path.resolve(options.i18nDir, './' + key + '/locale.js');
       utils.writeFile(tranPath, localeCode);
 
-      let outputXlsxPath = path.resolve(options.i18nDir, "./" + key + "/待翻译内容.xlsx");
+      let outputXlsxPath = path.resolve(options.i18nDir, './' + key + '/待翻译内容.xlsx');
       if (xlsxData.length) {
         let buf = utils.genXLSXData(xlsxData);
         utils.writeFile(outputXlsxPath, buf);
 
-        myOra.warn(xlsxData.length + "条待翻译数据，文件目录：");
-        myOra.warn(" > " + outputXlsxPath);
+        myOra.warn(xlsxData.length + '条待翻译数据，文件目录：');
+        myOra.warn(' > ' + outputXlsxPath);
       } else {
         utils.deleteFile(outputXlsxPath);
       }
