561c92efbbcf53eaa309b00253aa5cc5b5ea0231
mr18 <zhou060806@163.com>
2022-04-06 18:37:08 +0800

    Update utils.js

--- src/babel-plugin/utils.js
+++ src/babel-plugin/utils.js
@@ -33,7 +33,7 @@ module.exports.genUuidKey = genUuidKey;
  * @returns {boolean}
  */
 module.exports.isChinese = function (val) {
-  return /[\u4e00-\u9fa5]/.test(val);
+  return /[\u4e00-\u9fa5\u3002\uff1b\uff0c\uff1a\u201c\u201d\uff08\uff09\u3001\uff1f\u300a\u300b]/.test(val);
 };
 
 /**
