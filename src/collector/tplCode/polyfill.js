799228021e6ee4928241e95fe0f15386cb6f5329
mr18 <zhou060806@163.com>
2022-04-06 17:38:53 +0800

    Update polyfill.js

--- src/collector/tplCode/polyfill.js
+++ src/collector/tplCode/polyfill.js
@@ -1,15 +1,15 @@
 (function () {
   let _this = window;
-  _this.$LOCALE_VERSION = "${i18n_locale_language_version}";
-  _this.$LOCALE = {};
   let $i8n = function (key, val) {
-    return (_this.$LOCALE || {})[key] || val;
+    return ($i8n.package || {})[key] || val;
   };
   let $$i8n = function (val) {
     return val;
   };
+  $i8n.package = {};
+  $i8n.version = "${i18n_locale_language_version}";
   $i8n.locale = function (locale) {
-    _this.$LOCALE = locale || {};
+    $i8n.package = locale || {};
   };
   _this.$i8n = $i8n;
   _this.$$i8n = $$i8n;
